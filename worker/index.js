c48dd4bcce26a9bfe816cc9657a96f878e432c75
davidlacho <david.lacho@gmail.com>
2021-10-07 10:53:25 -0500

    Feature: Add Redis Worker

---
+++ worker/index.js
@@ -0,0 +1,21 @@
+// Connects to Redis
+const keys = require('./keys');
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+});
+const sub = redisClient.duplicate();
+
+// Calculating Fib Value
+const fib = (index) => index < 2 ? 1 : fib(index-1) + fib(index-2);
+
+// Watching for values
+sub.on('message', (channel, message)=> {
+    redisClient.hset('values', message, fib(parseInt(message)))
+});
+
+sub.subscribe('insert');
+
