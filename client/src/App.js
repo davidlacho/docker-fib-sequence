fcacad4a1fb893ef3ba57c1cf0134ead118380b5
davidlacho <david.lacho@gmail.com>
2021-11-08 09:42:23 -0800

    Make App Nicer

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,24 @@
+import React from 'react';
 import './App.css';
-import Fib from "./Fib";
-import OtherPage from "./OtherPage";
-import {BrowserRouter as Router, Link, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import OtherPage from './OtherPage';
+import Fib from './Fib';
 
 function App() {
-    return (
-        <>
-            <Router>
-                <header>
-                    <Link to="/">Home</Link>
-                    <Link to="/otherpage">Other Page</Link>
-                </header>
-                <Switch>
-                    <Route exact path="/"><Fib/></Route>
-                    <Route exact path="/otherpage"><OtherPage/></Route>
-                </Switch>
-
-            </Router>
-        </>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
